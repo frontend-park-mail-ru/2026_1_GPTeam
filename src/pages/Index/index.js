1aafd1ac1d161346c9c9c536ee84a41a1eb9ebb4
anonymous40000 <eurser@gmail.com>
2026-03-02 04:03:09 +0300

    landing and some other fixes

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,20 +1,9 @@
 import { BasePage } from "../base_page.js";
-import { Header } from "../../components/Header/header.js";
+import template from "./index.hbs?raw";
+import "./index.css";
 
 export class IndexPage extends BasePage {
     async render(root) {
-
-        root.innerHTML = `
-      <div class="page">
-        <header class="page__header"></header>
-        <main class="page__content">
-        
-        </main>
-      </div>
-    `;
-
-        const header = new Header({});
-        header.render(root.querySelector(".page__header"));
-        this._components.push(header);
+        root.innerHTML = template;
     }
 }
