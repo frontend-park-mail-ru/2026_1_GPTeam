6d1ea586ff75dc74e7f7b2da86584b7b5693e598
Timur <tima.temirov.2005@mail.ru>
2026-03-03 16:44:18 +0300

    Fixes (merge auth & signup).

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { LoginPage } from "./pages/Login/login.js";
 import { SignupPage } from "./pages/Signup/signup.js";
 import "./styles/global.css";
 import { LandingPage } from "./pages/Landing/landing.js";
+import {ProfilePage} from "./pages/Profile/profile.js";
 
 const root = document.getElementById("app");
 export const router = new Router(root);
@@ -10,7 +11,8 @@ export const router = new Router(root);
 router
     .addRoute("/", () => new LandingPage())
     .addRoute("/login", () => new LoginPage())
-    .addRoute("/signup", () => new SignupPage());
+    .addRoute("/signup", () => new SignupPage())
+    .addRoute("/profile", () => new ProfilePage());
 
 async function init() {
     router.start();
---
+++ src/pages/Profile/profile.js
@@ -0,0 +1,17 @@
+import {BasePage} from "../base_page.js";
+import {Header} from "../../components/Header/header.js";
+
+export class ProfilePage extends BasePage {
+    async render(root) {
+        root.innerHTML = `
+      <div class="page">
+        <header class="page__header"></header>
+        <main class="page__content"></main>
+      </div>
+    `;
+
+        const header = new Header({});
+        header.render(root.querySelector(".page__header"));
+        this._components.push(header);
+    }
+}
